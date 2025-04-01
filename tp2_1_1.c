e27c8855c855d64b4f011f99124692ceb2165a22
“Su <su>
2025-03-31 22:01:18 -0300

    Genera Numeros Aleatorios

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,10 +1,20 @@
-// codigo a completar
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>  
+
 #define N 20
-	
-int i;
-double vt[N];
-for(i = 0;i<N; i++)
-{ 
- 	    vt[i]=1+rand()%100;
-	    printf(“%f   ”, vt[i]);
+
+int main() {
+    int i;
+    double vt[N];
+
+    srand(time(NULL));
+
+    printf("Generando y mostrando %d números aleatorios:\n", N);
+    for(i = 0; i < N; i++) {
+        vt[i] = 1 + rand() % 100;
+        printf("%.2f  \n ", vt[i]);
+    }
+    printf("\n");
+    return 0;
 }
